9537e38683f2ee70835cc3e11b13902a8531d3eb
John Roller <jtroller33@gmail.com>
2023-04-04 21:31:59 -0500

    readme

--- README.md
+++ README.md
@@ -4,6 +4,11 @@
 ## Description
 
 This is a basic web crawler, and it's the first one I've made. It's built with Python 🐍 and its dependencies are `bs4` and `requests`.
+I created this to test the general mechanism of a web crawler, and it has a lot of room for improvement, outlined below:
+<br />
+
+***Please note:*** because this is a very basic crawler, it completely ignores `robots.txt` files, which are meant to instruct crawlers on how to operate on a particular site.  It also doesn't support parallelism, it takes ~1 sec to crawl each URL, and each time the crawler sends a request, it simply waits for the response and doesn't do anything else. It's not very efficient with a high number of URLS, doesn't remove query string parameters from the URLs, and doesn't handle relative or fragmented URLs.
+
 
 ## Basic Structure:
 ![Web Crawler Structure](./basic_structure.jpg)
@@ -19,7 +24,13 @@ This is a basic web crawler, and it's the first one I've made. It's built with P
 3. install dependencies in the venv:
 `pip install requests bs4`
 
-4. 
+4. decide what URL you want to start on, and add it to line 50:
+`Crawler(urls=['  <URL GOES HERE>  ']).run()`
+
+5. in your terminal, run:
+`python crawler.py`
+
+6. the crawler will log one line for each visited URL:
 
 
 
--- crawler.py
+++ crawler.py
@@ -11,14 +11,14 @@ logging.basicConfig(
 
 class Crawler:
 
-    def __init__(self, urls=[]):
-        self.visited_urls = []
-        self.urls_to_visit = urls
+    def __init__(self, urls=[]):        # constructor
+        self.visited_urls = []          # all URLs that have already been visited
+        self.urls_to_visit = urls       # URLs that need to be visited
 
     def download_url(self, url):
         return requests.get(url).text
 
-    def get_linked_urls(self, url, html):
+    def get_linked_urls(self, url, html):   # finds URLS on the site
         soup = BeautifulSoup(html, 'html.parser')
         for link in soup.find_all('a'):
             path = link.get('href')
@@ -26,11 +26,11 @@ class Crawler:
                 path = urljoin(url, path)
             yield path
 
-    def add_url_to_visit(self, url):
+    def add_url_to_visit(self, url):                # filters the URLs and adds them to 'urls_to_visit' if it hasn't been visited yet and isn't already in 'urls_to_visit'
         if url not in self.visited_urls and url not in self.urls_to_visit:
             self.urls_to_visit.append(url)
 
-    def crawl(self, url):
+    def crawl(self, url):                       # this is the method that the crawler actually uses
         html = self.download_url(url)
         for url in self.get_linked_urls(url, html):
             self.add_url_to_visit(url)
@@ -38,13 +38,13 @@ class Crawler:
     def run(self):
         while self.urls_to_visit:
             url = self.urls_to_visit.pop(0)
-            logging.info(f'Crawling: {url}')
+            logging.info(f'Crawling: {url}')        # logs URLs
             try:
                 self.crawl(url)
             except Exception:
                 logging.exception(f'Failed to crawl: {url}')
             finally:
-                self.visited_urls.append(url)
+                self.visited_urls.append(url)           # adds URL to 'visited_urls'
 
 if __name__ == '__main__':
-    Crawler(urls=['https://www.imdb.com/']).run()
+    Crawler(urls=['https://www.imdb.com/']).run()       # defines start URL
